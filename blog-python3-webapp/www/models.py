6df1917ac500a1aa1525f1f19965e6f8b35e9c7e
libingbin <libingbin2015@aliyun.com>
2017-08-06 21:41:39 +0800

    Models for user, blog, comment.

--- blog-python3-webapp/www/models.py
+++ blog-python3-webapp/www/models.py
@@ -5,15 +5,16 @@
     Models for user, blog, comment.
 '''
 
-
 __author__ = 'libingbin2015@aliyun.com'
 
 import time, uuid
 
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
+
 def next_id():
     return '%015%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
 
 class User(Model):
     __table__ = 'users'
@@ -25,6 +26,7 @@ class User(Model):
     name = StringField(ddl='varchar(50)')
     image = StringField(ddl='varchar(500)')
     created_at = FloatField(default=time.time)
+
 
 class Blog(Model):
     __table__ = 'blogs'
@@ -38,6 +40,7 @@ class Blog(Model):
     content = TextField()
     created_at = FloatField(default=time.time)
 
+
 class Comment(Model):
     __table__ = 'comments'
 
@@ -48,6 +51,3 @@ class Comment(Model):
     user_image = StringField(ddl='varchar(500)')
     content = TextField()
     created_at = FloatField(default=time.time)
-
-
-
